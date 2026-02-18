200d92fd8379995f85873609adc7d285d125d3b0
Carmine Giudice <c.giudice6@studenti.unisa.it>
2026-02-18 23:21:10 +0100

    C

--- js/profilo.js
+++ js/profilo.js
@@ -1,89 +1,90 @@
 const userIn = document.getElementById('username');
-        const emailIn = document.getElementById('email');
-        const passIn = document.getElementById('pass');
-        const btn = document.getElementById('btn-submit');
-        const tooltip = document.getElementById('password-tooltip');
-        const iconSlash = document.getElementById('icon-slash');
-        const iconEye = document.getElementById('icon-eye');
+const emailIn = document.getElementById('email');
+const passIn = document.getElementById('pass');
+const btn = document.getElementById('btn-submit');
+const tooltip = document.getElementById('password-tooltip');
+const iconSlash = document.getElementById('icon-slash');
+const iconEye = document.getElementById('icon-eye');
 
-        // GESTIONE TOOLTIP PASSWORD (SOTTO)
-        function showTooltip() {
-            tooltip.style.display = 'block';
-        }
-        function hideTooltip() {
-            tooltip.style.display = 'none';
-        }
+// GESTIONE TOOLTIP PASSWORD (SOTTO)
+function showTooltip() {
+    tooltip.style.display = 'block';
+}
 
-        passIn.addEventListener('mouseenter', showTooltip);
-        passIn.addEventListener('mouseleave', hideTooltip);
-        passIn.addEventListener('focus', showTooltip);
-        passIn.addEventListener('blur', hideTooltip);
+function hideTooltip() {
+    tooltip.style.display = 'none';
+}
 
-
-        function togglePassword() {
-            if (passIn.type === "password") {
-                passIn.type = "text";
-                iconSlash.hidden=true;
-                iconEye.hidden=false;
-
-            } else {
-                passIn.type = "password";
-                iconSlash.hidden=false;
-                iconEye.hidden=true;
-            }
-        }
-
-        function checkInputs() {
-            const passValue = passIn.value;
-            const emailValue = emailIn.value;
-
-            // Validazione Regex JS
-            const hasUpperCase = /[A-Z]/.test(passValue); 
-            const hasNumber = /[0-9]/.test(passValue);    
-            const hasSpecial = /[^a-zA-Z0-9]/.test(passValue); 
-            const hasLength = passValue.length >= 8;      
-            const emailRegex = /^[^\s@]+@[^\s@]+\.(com|it)$/; 
-
-            if (userIn.value.trim() !== "" && 
-                emailRegex.test(emailValue) && 
-                hasUpperCase && hasNumber && hasSpecial && hasLength) {
-                
-                btn.disabled = false;
-                btn.style.opacity = "1";
-            } else {
-                btn.disabled = true;
-                btn.style.opacity = "0.5";
-            }
-        }
-
-        userIn.addEventListener('input', checkInputs);
-        emailIn.addEventListener('input', checkInputs);
-        passIn.addEventListener('input', checkInputs);
-        
-        checkInputs();
+passIn.addEventListener('mouseenter', showTooltip);
+passIn.addEventListener('mouseleave', hideTooltip);
+passIn.addEventListener('focus', showTooltip);
+passIn.addEventListener('blur', hideTooltip);
 
 
-let map = null; 
+function togglePassword() {
+    if (passIn.type === "password") {
+        passIn.type = "text";
+        iconSlash.hidden = true;
+        iconEye.hidden = false;
 
-    function getLocation() {
-      
-      const mapContainer = document.getElementById("map");
+    } else {
+        passIn.type = "password";
+        iconSlash.hidden = false;
+        iconEye.hidden = true;
+    }
+}
 
-      navigator.geolocation.getCurrentPosition(
+function checkInputs() {
+    const passValue = passIn.value;
+    const emailValue = emailIn.value;
+
+    // Validazione Regex JS
+    const hasUpperCase = /[A-Z]/.test(passValue);
+    const hasNumber = /[0-9]/.test(passValue);
+    const hasSpecial = /[^a-zA-Z0-9]/.test(passValue);
+    const hasLength = passValue.length >= 8;
+    const emailRegex = /^[^\s@]+@[^\s@]+\.(com|it)$/;
+
+    if (userIn.value.trim() !== "" &&
+        emailRegex.test(emailValue) &&
+        hasUpperCase && hasNumber && hasSpecial && hasLength) {
+
+        btn.disabled = false;
+        btn.style.opacity = "1";
+    } else {
+        btn.disabled = true;
+        btn.style.opacity = "0.5";
+    }
+}
+
+userIn.addEventListener('input', checkInputs);
+emailIn.addEventListener('input', checkInputs);
+passIn.addEventListener('input', checkInputs);
+
+checkInputs();
+
+
+let map = null;
+
+function getLocation() {
+
+    const mapContainer = document.getElementById("map");
+
+    navigator.geolocation.getCurrentPosition(
         (position) => {
-          const lat = position.coords.latitude;
-          const lon = position.coords.longitude;
+            const lat = position.coords.latitude;
+            const lon = position.coords.longitude;
 
-          mapContainer.style.display = 'block';
+            mapContainer.style.display = 'block';
 
-          map = L.map('map').setView([lat, lon], 11); 
-          L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-              maxZoom: 19
-          }).addTo(map);
+            map = L.map('map').setView([lat, lon], 11);
+            L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                maxZoom: 19
+            }).addTo(map);
 
-          const suite = L.marker([40.6780, 14.7625]).addTo(map).bindPopup("<b>Suite!</b>").openPopup();
-          const deluxe = L.marker([40.67891, 14.75808]).addTo(map).bindPopup("<b>Deluxe!</b>").openPopup();
-          const marker = L.marker([lat, lon]).addTo(map).bindPopup("<b>Sei qui!</b>").openPopup();          
+            const suite = L.marker([40.6780, 14.7625]).addTo(map).bindPopup("<b>Suite!</b>").openPopup();
+            const deluxe = L.marker([40.67891, 14.75808]).addTo(map).bindPopup("<b>Deluxe!</b>").openPopup();
+            const marker = L.marker([lat, lon]).addTo(map).bindPopup("<b>Sei qui!</b>").openPopup();
         }
-      );
-    }        
+    );
+}
