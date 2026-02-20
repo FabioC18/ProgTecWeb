21c275cac84ada2b1cc6e7f1a20c3c61a9014513
MastrogiacomoFrancescoPio <f.mastrogiacomo1@studenti.unisa.it>
2026-02-20 18:44:09 +0100

    prezzi pacchetti cambiati

--- script_db/pacchetti_home.sql
+++ script_db/pacchetti_home.sql
@@ -11,7 +11,7 @@ ALTER TABLE pacchetti OWNER TO www;
 
 
 INSERT INTO pacchetti (nome, descrizione, prezzo, immagine) VALUES 
-('Pacchetto Base', 'Allestimento love standard per una serata romantica.', 49.99, 'packbase.jpeg'),
-('Love Base', 'Allestimento + aperitivo con stuzzichini locali.', 79.99, 'love-base.jpeg'),
-('Love Plus', 'Allestimento + aperitivo large con bottiglia di spumante.', 119.99, 'ape3.jpeg'),
-('Love Sushi', 'Allestimento + box sushi speciale per due persone.', 149.99, 'imgsushi.jpeg');
+('Pacchetto Base', 'Allestimento love standard per una serata romantica.', 19, 'packbase.jpeg'),
+('Love Base', 'Allestimento + aperitivo con stuzzichini locali.', 29, 'love-base.jpeg'),
+('Love Plus', 'Allestimento + aperitivo large con bottiglia di spumante.', 39, 'ape3.jpeg'),
+('Love Sushi', 'Allestimento + box sushi speciale per due persone.', 49, 'imgsushi.jpeg');
